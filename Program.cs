88ab9336a645362e9219ed5abe41847c16e3b4af
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-07-19 10:59:45 +0300

    Papildomos uzduotys2

--- Program.cs
+++ Program.cs
@@ -115,13 +115,14 @@ namespace ManoPrograma
         //public static void Main(string[] args)
         //{
         //    Console.WriteLine("Iveskite valanda: ");
+        //    int valanda = int.Parse(Console.ReadLine());
         //    Console.WriteLine("Iveskite minutes: ");
-        //    Console.WriteLine(ArLaikas(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        //    Console.WriteLine(ArLaikas(valanda, int.Parse(Console.ReadLine())));
         //    Console.WriteLine(ArLaikas);
         //}
         //public static string ArLaikas(int skaicius1, int skaicius2)
         //{
-        //    if (skaicius1>0 && skaicius1<23 && skaicius2>0 && skaicius2<59)
+        //    if (skaicius1 > 0 && skaicius1 < 23 && skaicius2 > 0 && skaicius2 < 59)
         //    {
         //        return "Teisinga įvestis";
         //    }
@@ -132,7 +133,83 @@ namespace ManoPrograma
         //}
 
 
+        //***Užduotis 8
+        ////
 
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite kvadrato krastine: ");
+        //    double perimetras = Krastine(double.Parse(Console.ReadLine()));
+        //    Console.WriteLine("Kvadrato perimetras: " + perimetras);
+        //}
+        //public static double Krastine(double skaicius)
+        //{
+        //    return skaicius * 4;
+        //}
+
+        //***Užduotis 9
+        //
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite trikampio ilgi : ");
+        //    double ilgis = double.Parse(Console.ReadLine());
+        //    Console.WriteLine("Iveskite trikampio auksti : ");
+        //    double plotas = Krastines(ilgis, double.Parse(Console.ReadLine()));
+        //    Console.WriteLine("Trikampio plotas: " + plotas);
+        //}
+        //public static double Krastines(double skaicius1, double skaicius2)
+        //{
+        //    return (skaicius1 * skaicius2) / 2;
+        //}
+
+        //***Užduotis 10
+        //
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite apskritimo spinduli : ");
+        //    double plotas = SkaiciuokPlota(double.Parse(Console.ReadLine()));
+        //    double perimetras = SkaiciuokPerimetra(double.Parse(Console.ReadLine()));
+        //    Console.WriteLine("Apskritimo plotas: " + plotas);
+        //    Console.WriteLine("Apskritimo perimetras: " + perimetras);
+        //}
+        //public static double SkaiciuokPlota (double skaicius)
+        //{
+        //    return (Math.PI * (skaicius * skaicius));
+        //}
+        //public static double SkaiciuokPerimetra (double skaicius)
+        //{
+        //    return (2* Math.PI * skaicius);
+        //}
+
+        //***Užduotis 11
+        //
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite kubo krastines ilgi : ");
+        //    double turis = SkaiciuokTuri(double.Parse(Console.ReadLine()));
+        //    Console.WriteLine("Kubo turis: " + turis);
+        //}
+        //public static double SkaiciuokTuri(double skaicius)
+        //{
+        //    return skaicius * skaicius * skaicius;
+        //}
+
+        //***Užduotis 12
+        //
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite temperatura Celsijaus : ");
+        //    double Farenheitas = SkaiciuokTemperatura(double.Parse(Console.ReadLine()));
+        //    Console.WriteLine("Temperatura Farenheitu: " + Farenheitas);
+        //}
+        //public static double SkaiciuokTemperatura(double skaicius)
+        //{
+        //    return (skaicius * 1.8) + 32;
+        //}
 
 
     }
