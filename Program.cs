143793cd631e099f6378551c0cb3528b6635a99b
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-07-19 08:55:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection;
+
+namespace ManoPrograma
+{
+    public class PagrindineKlase
+    {
+        //***Užduotis 1
+
+        //public static void Main1(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite skaiciu: ");
+        //    string rezultatas = ArTeigiamas(int.Parse(Console.ReadLine()));
+        //    Console.WriteLine(rezultatas);
+        //}
+        //public static string ArTeigiamas(int skaicius)
+        //{
+        //    if (skaicius > 0)
+        //        return "Teigiamas";
+        //    else if (skaicius == 0)
+        //        return "Nulis";
+        //    return "Neigiamas";
+        //}
+
+        //***Užduotis 2
+
+        //public static void Main2(int[] args)
+        //{
+        //    Console.WriteLine("Iveskite pirma skaiciu:: ");
+        //    Console.WriteLine("Iveskite antra skaiciu:: ");
+        //    Console.WriteLine("Iveskite trecia skaiciu:: ");
+        //    Console.WriteLine(Didziausias1(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+
+        //}
+        //public static int Didziausias1(int skaicius1, int skaicius2, int skaicius3)
+        //{
+        //    if (skaicius1 > skaicius2 && skaicius1 > skaicius3)
+        //    {
+        //        return skaicius1;
+        //    }
+        //    else if (skaicius2 > skaicius1 && skaicius2 > skaicius3)
+        //    {
+        //        return skaicius2;
+        //    }
+        //    else
+        //    {
+        //        return skaicius3;
+        //    }
+        //}
+
+        //***Užduotis 3
+
+        //public static void Main3(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite raide: ");
+        //    string rezultatas = ArBalse(Console.ReadLine()?.ToLower());
+        //    Console.WriteLine(rezultatas);
+        //}
+        //public static string ArBalse(string raide)
+        //{
+        //    if (raide == "a" || raide == "e" || raide == "i" || raide == "y" || raide == "o" || raide == "u")
+        //    {
+        //        return "Balse";
+        //    }
+        //    else
+        //    {
+        //        return "Priebalse";
+        //    }
+        //}
+
+        //***Užduotis 4
+
+        //public static void Main4(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite metus: ");
+        //    Console.WriteLine(ArKeliamieji(int.Parse(Console.ReadLine())));
+        //    Console.WriteLine(ArKeliamieji);
+        //}
+        //public static string ArKeliamieji(int metai)
+        //{
+        //    if (metai % 4 == 0)
+        //    {
+        //        return "Keliamieji";
+        //    }
+        //    else
+        //    {
+        //        return "Nekeliamieji";
+        //    }
+
+        //}
+
+        //***Užduotis 5
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite norima simboli: ");
+        //    Console.WriteLine(ArSkaicius(Console.ReadLine()[0]));
+        //    Console.WriteLine(ArSkaicius);
+        //}
+        //public static string ArSkaicius(char simbolis)
+        //{
+        //    if (char.IsDigit (simbolis))
+
+        //        {
+        //        return "skaicius";
+        //    }
+        //    else
+        //    {
+        //        return "raide";
+        //    }
+
+        //***Užduotis 6
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Iveskite valanda: ");
+        //    Console.WriteLine("Iveskite minutes: ");
+        //    Console.WriteLine(ArLaikas(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        //    Console.WriteLine(ArLaikas);
+        //}
+        //public static string ArLaikas(int skaicius1, int skaicius2)
+        //{
+        //    if (skaicius1>0 && skaicius1<23 && skaicius2>0 && skaicius2<59)
+        //    {
+        //        return "Teisinga įvestis";
+        //    }
+        //    else
+        //    {
+        //        return "Neteisinga įvestis";
+        //    }
+        //}
+
+
+
+
+
+    }
+
+
+}
+
